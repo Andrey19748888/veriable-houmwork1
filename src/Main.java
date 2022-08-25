9a0d170a27a0d88dc639dceec6ad4112d659064b
Andreydenisov7788 <asasa88881974@mail.ru>
2022-08-25 18:28:56 +0300

    переменная задача 6

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        var sum = boxerWeight1 +boxerWeight2;
+        System.out.println(" общий вес боксеров" + sum + "кг");
+        var giff = boxerWeight2 - boxerWeight1;
+        System.out.println("разница в весе боксеров" + giff + "кг");
+
     }
 }
