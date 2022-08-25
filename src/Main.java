881c0e16a9e3e3a2a855ca18b7bc35442f56b756
Andreydenisov7788 <asasa88881974@mail.ru>
2022-08-25 23:01:51 +0300

    переменная задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
+        var hoursCommon = 640;
+        var workingDay = 8;
+        var ammountWorkers = hoursCommon / workingDay;
+        System.out.println(" Всего работников в компании " +   ammountWorkers   +   " человек ");
+        var newPeople = 94;
+        var ammountWorkers2 = ammountWorkers + newPeople;
+        System.out.println(" работает работников " + ammountWorkers2);
+        var  hoursCommon2 = hoursCommon / ammountWorkers2;
+        System.out.println(" Если в компании работает 174 человека, то всего " + hoursCommon2 + " часа работы может быть поделено между сотрудниками  ");
+
+
     }
 }
